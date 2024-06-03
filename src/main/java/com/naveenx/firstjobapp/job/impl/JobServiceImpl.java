214ab622a82e60ef8bca08aa5f353f6d2a0dd040
Naveen Kumar Gavara <gnaveenkumar316@gmail.com>
2024-06-02 19:25:05 -0500

    Refactor controller for loose coupling and add logic to handle job ID

--- src/main/java/com/naveenx/firstjobapp/job/impl/JobServiceImpl.java
+++ src/main/java/com/naveenx/firstjobapp/job/impl/JobServiceImpl.java
@@ -2,21 +2,25 @@ package com.naveenx.firstjobapp.job.impl;
 
 import com.naveenx.firstjobapp.job.Job;
 import com.naveenx.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class JobServiceImpl implements JobService {
 
     private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Job> findAll() {
-        return null;
+        return jobs;
     }
 
     @Override
     public void createJob(Job job) {
+        job.setId(nextId++);
         jobs.add(job);
     }
 }
